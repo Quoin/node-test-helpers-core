45f4db0ad6d1227a08d35c290143dd3d0deb8a40
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-02-21 23:37:06 -0500

    Adding node-mocks-http

--- grunt/config/eslint.js
+++ grunt/config/eslint.js
@@ -2,6 +2,20 @@ module.exports = {
     options: {
         fix: true,
     },
+
+    dev: {
+        options: {
+            rules: {
+                'import/no-extraneous-dependencies': 0,
+                indent: [2, 4],
+            },
+        },
+        src: [
+            '*.js',
+            'grunt/**/*.js',
+        ],
+    },
+
     node: {
         options: {
             envs: [
@@ -11,22 +25,24 @@ module.exports = {
                 modules: false,
             },
             rules: {
-                'import/no-extraneous-dependencies': 0,
                 indent: [2, 4],
             },
         },
         src: [
-            '*.js',
-            'grunt/**/*.js',
             'lib/**/*.js',
             '!lib/**/*.spec.*.js',
         ],
     },
+
     nodeTest: {
         options: {
             envs: [
                 'mocha',
             ],
+            rules: {
+                'import/no-extraneous-dependencies': 0,
+                indent: [2, 4],
+            },
         },
         src: [
             'lib/**/*.spec.*.js',
